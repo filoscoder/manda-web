6d9acea76bdd889093b0df551390a38904b111c1
filoscoder <sondaniel.88@gmail.com>
2020-03-04 21:52:08 +0900

    created dndUtil style helper

---
+++ src/utils/dndUtil.js
@@ -0,0 +1,36 @@
+// a little function to help us with reordering the result
+export const Reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 6;
+export const getItemStyle = (isDragging, draggableStyle) => {
+  return {
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+    textAlign: "right",
+
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+  };
+};
+
+export const getListDraggingStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightgray" : "white",
+  padding: 8,
+  width: 350
+});
+
+export const getCardDraggingStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightgray" : "transparent",
+  width: 275
+});
