41323bee8b1ab8529a28d4a9df35104f7805c4a1
filoscoder <sondaniel.88@gmail.com>
2020-04-18 17:36:33 +0900

    added login background image

--- src/components/common/LogIn.js
+++ src/components/common/LogIn.js
@@ -4,8 +4,9 @@ import { withRouter, Link, Redirect } from "react-router-dom";
 import { Auth } from "../../setup/firebase";
 import { AuthContext } from "../../helpers/Auth";
 // Style & UI Framework imports
-import { Form, Input, Button, message } from "antd";
+import { Form, Input, Button, message, Tag } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import background from "../../assets/login-back.jpeg";
 
 const layout = {
   labelCol: {
@@ -74,67 +75,76 @@ const LogIn = ({ history, props }) => {
       <div
         style={{
           textAlign: "center",
-          fontSize: "2rem",
-          marginTop: "90px",
+          fontSize: "2.3rem",
           marginLeft: "auto",
           marginRight: "auto",
+          minHeight: "inherit",
+          backgroundSize: "cover",
+          backgroundPosition: "center center no-repeat",
+          backgroundImage: `url(${background})`,
         }}
       >
-        <h1>Test-Auth-LogIn</h1>
-        <div>
-          <Form
-            {...layout}
-            name="login-form"
-            initialValues={{ remember: true }}
-            onFinish={onSubmitForm}
-            style={{
-              display: "inline-block",
-              marginTop: "50px",
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <Form.Item
-              name="email"
-              label="Username"
-              rules={[{ required: true, message: "Please enter your [email]" }]}
-            >
-              <Input prefix={<UserOutlined />} placeholder="my-email@com.com" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              label="Password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please enter at least 6 characters",
-                },
-              ]}
-            >
-              <Input.Password
-                prefix={<LockOutlined />}
-                type="password"
-                placeholder="my-password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                loading={isLoading}
-              >
-                Login
-              </Button>
-              <br />
-              <br />
-              or{" "}
-              <Link to="/signup">
-                <a href="">[Sign-up] here!</a>
-              </Link>
-            </Form.Item>
-          </Form>
+        <div style={{ color: "#3b3a3f", padding: "50px" }}>
+          <h1>Welcome to Manda-web!</h1>
+          <h6>Please login with your email and password</h6>
         </div>
+
+        <Form
+          {...layout}
+          name="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onSubmitForm}
+          style={{
+            display: "inline-block",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+          hideRequiredMark
+          colon={false}
+        >
+          <Form.Item
+            name="email"
+            label="Username"
+            rules={[{ required: true, message: "Please enter your [email]" }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="my-email@com.com" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter at least 6 characters",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="my-password"
+            />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              loading={isLoading}
+            >
+              Log in
+            </Button>
+            <br />
+            <br />
+          </Form.Item>
+          or{" "}
+          <Link to="/signup">
+            <Tag color="#3b5999" style={{ cursor: "pointer" }}>
+              Sign-up
+            </Tag>
+          </Link>
+          here!
+        </Form>
       </div>
     );
   }
